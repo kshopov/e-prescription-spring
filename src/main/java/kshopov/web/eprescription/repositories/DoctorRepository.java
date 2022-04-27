2752de63fa8ae46a3284bd82c51e0bbe95fbac10
kshopov <krasimir.shopov@gmail.com>
2022-04-27 13:29:39 +0300

    Revert "Revert "added spring security""
    This reverts commit 2981a718544d8077feb92fa9579de740a996a8a5.

---
+++ src/main/java/kshopov/web/eprescription/repositories/DoctorRepository.java
@@ -0,0 +1,9 @@
+package kshopov.web.eprescription.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kshopov.web.eprescription.model.Doctor;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+}
