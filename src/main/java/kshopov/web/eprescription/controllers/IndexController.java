a7b9298b612ebf30de375b3f575eda030412afd1
kshopov <krasimir.shopov@gmail.com>
2022-04-27 12:08:50 +0300

    added spring security

--- src/main/java/kshopov/web/eprescription/controllers/IndexController.java
+++ src/main/java/kshopov/web/eprescription/controllers/IndexController.java
@@ -10,10 +10,17 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kshopov.web.eprescription.model.Doctor;
+import kshopov.web.eprescription.repositories.DoctorRepository;
 
 @Controller
 public class IndexController implements WebMvcConfigurer{
 	
+	private final DoctorRepository doctorRepository;
+	
+	public IndexController(DoctorRepository doctorRepository) {
+		this.doctorRepository = doctorRepository;
+	}
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("successful_registration").setViewName("index/successful_registration");
@@ -29,12 +36,15 @@ public class IndexController implements WebMvcConfigurer{
 		return "index/register";
 	}
 	
-	@PostMapping({"register", "register.html"})
-	public String validateUserInfo(@Valid Doctor doctor, 
+	@PostMapping({"process_register", "process_register.html"})
+	public String processRegistration(@Valid Doctor doctor, 
 			BindingResult bindingResult) {
+		
 		if(bindingResult.hasErrors()) {
 			return "index/register";
 		}
+		
+		doctorRepository.save(doctor);
 		
 		return "redirect:/successful_registration";
 	}
