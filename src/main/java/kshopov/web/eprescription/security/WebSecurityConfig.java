671c27e9ba3539a266a11a7db4258dba0f5c332a
kshopov <krasimir.shopov@gmail.com>
2022-04-29 13:44:53 +0300

    added password encoder class

--- src/main/java/kshopov/web/eprescription/security/WebSecurityConfig.java
+++ src/main/java/kshopov/web/eprescription/security/WebSecurityConfig.java
@@ -1,14 +1,11 @@
 package kshopov.web.eprescription.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -16,16 +13,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
     @Autowired
     private UserDetailsService userDetailsService;
+    
+    private final PasswordEncoder passwordEncoder;
 	
-	public WebSecurityConfig() {
-		super();
+	public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { 
 		auth
 			.userDetailsService(userDetailsService)
-			.passwordEncoder(this.passwordEncoder());
+			.passwordEncoder(passwordEncoder.bCryptPasswordEncoder());
 	}
 	
 	@Override
@@ -41,8 +40,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl("/doLogin");
 	}// @formatter:on
 	
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
