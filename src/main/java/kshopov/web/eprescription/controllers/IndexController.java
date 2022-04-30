f2dc62d0e876015220185ca2fe8f5a37192f9c37
kshopov <krasimir.shopov@gmail.com>
2022-04-30 13:01:51 +0300

    added sending email service

--- src/main/java/kshopov/web/eprescription/controllers/IndexController.java
+++ src/main/java/kshopov/web/eprescription/controllers/IndexController.java
@@ -16,6 +16,7 @@ import kshopov.web.eprescription.model.VerificationToken;
 import kshopov.web.eprescription.repositories.DoctorRepository;
 import kshopov.web.eprescription.repositories.VerificationTokenRepository;
 import kshopov.web.eprescription.services.DoctorService;
+import kshopov.web.eprescription.services.EmailService;
 import kshopov.web.eprescription.validation.EmailExistsException;
 
 @Controller
@@ -23,14 +24,17 @@ public class IndexController implements WebMvcConfigurer{
 	
 	private final DoctorRepository doctorRepository;
 	private final VerificationTokenRepository verificationTokenRepository;
+	private final EmailService emailService;
 	
 	private final DoctorService doctorService;
 	
 	public IndexController(DoctorRepository doctorRepository, DoctorService doctorService,
-			VerificationTokenRepository verificationTokenRepository) {
+			VerificationTokenRepository verificationTokenRepository,
+			EmailService emailService) {
 		this.doctorRepository = doctorRepository;
 		this.doctorService = doctorService;
 		this.verificationTokenRepository = verificationTokenRepository;
+		this.emailService = emailService;
 	}
 
 	@Override
@@ -68,8 +72,15 @@ public class IndexController implements WebMvcConfigurer{
 		final String token = UUID.randomUUID().toString();
 		final VerificationToken myToken = new VerificationToken(token, doctor);
 		verificationTokenRepository.save(myToken);
+
+		emailService.sendSimpleMessage(doctor.getEmail(), "test messsage", 
+				"Това е тестово съобщение " + this.generateUrl());
 		
 		return "redirect:/login";
+	}
+	
+	private String generateUrl() {
+		return "url";
 	}
 	
 }
