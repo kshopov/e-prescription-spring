bfca82ffca583eca2647687c059ad8fea622db27
kshopov <krasimir.shopov@gmail.com>
2022-05-01 13:53:54 +0300

    added confirmRegistration method, catching email exist exception

--- src/main/java/kshopov/web/eprescription/controllers/RegistrationController.java
+++ src/main/java/kshopov/web/eprescription/controllers/RegistrationController.java
@@ -6,13 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kshopov.web.eprescription.model.Doctor;
 import kshopov.web.eprescription.model.VerificationToken;
-import kshopov.web.eprescription.repositories.DoctorRepository;
 import kshopov.web.eprescription.repositories.VerificationTokenRepository;
 import kshopov.web.eprescription.services.DoctorService;
 import kshopov.web.eprescription.services.EmailService;
@@ -21,16 +25,15 @@ import kshopov.web.eprescription.validation.EmailExistsException;
 @Controller
 public class RegistrationController {
 	
-	private final DoctorRepository doctorRepository;
 	private final VerificationTokenRepository verificationTokenRepository;
+
 	private final EmailService emailService;
 	
 	private final DoctorService doctorService;
 	
-	public RegistrationController(DoctorRepository doctorRepository, DoctorService doctorService,
+	public RegistrationController(DoctorService doctorService,
 			VerificationTokenRepository verificationTokenRepository,
 			EmailService emailService) {
-		this.doctorRepository = doctorRepository;
 		this.doctorService = doctorService;
 		this.verificationTokenRepository = verificationTokenRepository;
 		this.emailService = emailService;
@@ -42,29 +45,57 @@ public class RegistrationController {
 	}
 	
 	@PostMapping({"register"})
-	public String processRegistration(@Valid Doctor doctor, 
+	public String register(@Valid Doctor doctor, 
 			BindingResult bindingResult, final HttpServletRequest request) throws EmailExistsException {
 		
 		if(bindingResult.hasErrors()) {
 			return "index/register";
 		}
-		
-		doctor.setVerified(false);
-		doctorService.registerNewUser(doctor);
-		
-		
-		final String token = UUID.randomUUID().toString();
-		final VerificationToken myToken = new VerificationToken(token, doctor);
-		verificationTokenRepository.save(myToken);
 
-		emailService.sendSimpleMessage(doctor.getEmail(), "Потвърждение на акаунт", 
-				"На следния адрес може да потвърдите Вашия акаунт " + this.generateUrl(request));
-		
+		try {
+			doctor.setVerified(false);
+			doctorService.registerNewUser(doctor);
+			
+			final String token = UUID.randomUUID().toString();
+			final VerificationToken myToken = new VerificationToken(token, doctor);
+			verificationTokenRepository.save(myToken);
+	
+			emailService.sendSimpleMessage(doctor.getEmail(), "Потвърждение на акаунт", 
+					"На следния адрес може да потвърдите Вашия акаунт " + this.generateUrl(request, token));
+		} catch (EmailExistsException e) {
+			bindingResult.addError(new FieldError("doctor", "email", e.getMessage()));
+			return "index/register";
+		}
+
 		return "redirect:/login";
 	}
 	
-	private String generateUrl(HttpServletRequest request) {
-		return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+	@GetMapping(value = "registrationConfirm")
+	public ModelAndView confirmRegistration(final Model model, 
+		@RequestParam("token") final String token, 
+		final RedirectAttributes redirectAttributes) {
+			final VerificationToken verificationToken = doctorService.getVerificationToken(token);
+			if(verificationToken == null) {
+				redirectAttributes.addFlashAttribute("errorMessage", "Невалиден токен!");
+				return new ModelAndView("redirect:/login");
+			}
+
+		final Doctor doctor = verificationToken.getDoctor();
+			doctor.setVerified(true);
+			doctorService.saveRegisteredUser(doctor);
+
+			redirectAttributes.addFlashAttribute("message", "Акаунтът Ви е успешно потвърден!");
+
+			return new ModelAndView("redirect:/login");
+	}
+
+
+	private String generateUrl(HttpServletRequest request, String token) {
+		return "http://" + request.getServerName() 
+			+ ":" + request.getServerPort() 
+			+ request.getContextPath() 
+			+ "/registrationConfirm?token="
+			+ token;
 	}
 
 }
