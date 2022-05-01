6649f510caceb7a576617777294350c39e8ac800
kshopov <krasimir.shopov@gmail.com>
2022-05-01 13:51:17 +0300

    added getVerificationToken and saveRegisteredUser methods

--- src/main/java/kshopov/web/eprescription/services/DoctorService.java
+++ src/main/java/kshopov/web/eprescription/services/DoctorService.java
@@ -1,6 +1,7 @@
 package kshopov.web.eprescription.services;
 
 import kshopov.web.eprescription.model.Doctor;
+import kshopov.web.eprescription.model.VerificationToken;
 import kshopov.web.eprescription.validation.EmailExistsException;
 
 public interface DoctorService {
@@ -8,5 +9,9 @@ public interface DoctorService {
     Doctor registerNewUser(Doctor doctor) throws EmailExistsException;
 
     Doctor updateExistingUser(Doctor doctor) throws EmailExistsException;
+
+    VerificationToken getVerificationToken(String token);
+
+    void saveRegisteredUser(Doctor doctor);
 
 }
--- src/main/java/kshopov/web/eprescription/services/DoctorServiceImpl.java
+++ src/main/java/kshopov/web/eprescription/services/DoctorServiceImpl.java
@@ -5,7 +5,9 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import kshopov.web.eprescription.model.Doctor;
+import kshopov.web.eprescription.model.VerificationToken;
 import kshopov.web.eprescription.repositories.DoctorRepository;
+import kshopov.web.eprescription.repositories.VerificationTokenRepository;
 import kshopov.web.eprescription.security.PasswordEncoder;
 import kshopov.web.eprescription.validation.EmailExistsException;
 
@@ -13,32 +15,47 @@ import kshopov.web.eprescription.validation.EmailExistsException;
 @Transactional
 public class DoctorServiceImpl implements DoctorService {
 	
-    private final DoctorRepository repository;
+    private final DoctorRepository doctorRepository;
     
     private final PasswordEncoder passwordEncoder;
 
-	public DoctorServiceImpl(DoctorRepository repository, PasswordEncoder passwordEncoder) {
-		this.repository = repository;
+    private final VerificationTokenRepository verificationTokenRepo;
+
+	public DoctorServiceImpl(DoctorRepository repository, 
+                            PasswordEncoder passwordEncoder,
+                            VerificationTokenRepository verificationTokenRepo) {
+		this.doctorRepository = repository;
 		this.passwordEncoder = passwordEncoder;
+        this.verificationTokenRepo = verificationTokenRepo;
 	}
 
 	@Override
 	public Doctor registerNewUser(Doctor doctor) throws EmailExistsException {
         if (emailExist(doctor.getEmail())) {
-            throw new EmailExistsException("There is an account with that email address: " + doctor.getEmail());
+            throw new EmailExistsException("Вече има създаден акаунт с този email адрес: " + doctor.getEmail());
         }
         doctor.setPassword(passwordEncoder.bCryptPasswordEncoder().encode(doctor.getPassword()));
-        return repository.save(doctor);
+        return doctorRepository.save(doctor);
 	}
 
 	@Override
 	public Doctor updateExistingUser(Doctor doctor) throws EmailExistsException {
 		return null;
 	}
-	
-    private boolean emailExist(String email) {
-        final Doctor user = repository.findByEmail(email);
-        return user != null;
+    
+    @Override
+    public VerificationToken getVerificationToken(String token) {
+        return verificationTokenRepo.findByToken(token);
+    }
+
+    @Override
+    public void saveRegisteredUser(Doctor doctor) {
+        doctorRepository.save(doctor);
     }
 	
+    private boolean emailExist(String email) {
+        final Doctor user = doctorRepository.findByEmail(email);
+        return user != null;
+    }
+
 }
