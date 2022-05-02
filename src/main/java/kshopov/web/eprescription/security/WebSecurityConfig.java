555282999cad96d25f034650c47164176607cb0b
kshopov <krasimir.shopov@gmail.com>
2022-05-02 09:41:40 +0300

    added logout functionality

--- src/main/java/kshopov/web/eprescription/security/WebSecurityConfig.java
+++ src/main/java/kshopov/web/eprescription/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -37,7 +38,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 			.formLogin()
 				.loginPage("/login").permitAll()
-				.loginProcessingUrl("/doLogin");
+				.loginProcessingUrl("/doLogin")
+			.and()
+				.logout().permitAll().logoutRequestMatcher(new AntPathRequestMatcher("/doLogout", "GET"));
 	}// @formatter:on
 	
 }
