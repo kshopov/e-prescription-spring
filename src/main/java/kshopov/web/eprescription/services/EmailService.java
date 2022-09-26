2d97372380bb70a6588147fba3352fecc70b315b
kshopov <krasimir.shopov@gmail.com>
2022-09-26 15:14:22 +0300

    url generator method moved from controller to service

--- src/main/java/kshopov/web/eprescription/services/EmailService.java
+++ src/main/java/kshopov/web/eprescription/services/EmailService.java
@@ -1,7 +1,10 @@
 package kshopov.web.eprescription.services;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface EmailService {
 
 	void sendSimpleMessage(String to, String subject, String text);
-	
+
+	String urlGenerator(HttpServletRequest request, String url, String token);
 }
