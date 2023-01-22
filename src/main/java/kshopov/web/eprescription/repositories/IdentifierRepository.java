720e0c42c5224b4b3c59c3368e22e5d281f0d01d
kshopov <krasimir.shopov@gmail.com>
2023-01-22 16:13:29 +0200

    changed jpa repo to crud repo

--- src/main/java/kshopov/web/eprescription/repositories/IdentifierRepository.java
+++ src/main/java/kshopov/web/eprescription/repositories/IdentifierRepository.java
@@ -1,7 +1,7 @@
 package kshopov.web.eprescription.repositories;
 
 import kshopov.web.eprescription.model.Identifier;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IdentifierRepository extends JpaRepository<Identifier, Long> {
+public interface IdentifierRepository extends CrudRepository<Identifier, Long> {
 }
