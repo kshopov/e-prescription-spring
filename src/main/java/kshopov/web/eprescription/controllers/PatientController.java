8a6cab26864502535f1ae4e395e2bd0f2f4ecc36
kshopov <krasimir.shopov@gmail.com>
2023-01-29 13:13:10 +0200

    added post and get methods

--- src/main/java/kshopov/web/eprescription/controllers/PatientController.java
+++ src/main/java/kshopov/web/eprescription/controllers/PatientController.java
@@ -1,13 +1,28 @@
 package kshopov.web.eprescription.controllers;
 
+import kshopov.web.eprescription.model.Patient;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class PatientController {
 
     @GetMapping({  "addpatient"})
-    public String addPatient() {
+    public String addPatientForm(Model model) {
+        System.out.println("Get add patient");
+        return "patient/addpatient";
+    }
+
+    @PostMapping({"addpatient"})
+    public String addPatientSubmit(@Valid Patient patient,
+                                   BindingResult bindingResult) {
+        System.out.println("Post add patient");
+
         return "patient/addpatient";
     }
 
