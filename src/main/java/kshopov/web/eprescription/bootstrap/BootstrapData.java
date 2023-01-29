131bd201dfe61c1ad10a69d4e8ab948bf87c83bd
kshopov <krasimir.shopov@gmail.com>
2023-01-29 15:43:51 +0200

    Identifier model renamed to IdentifierType

--- src/main/java/kshopov/web/eprescription/bootstrap/BootstrapData.java
+++ src/main/java/kshopov/web/eprescription/bootstrap/BootstrapData.java
@@ -1,6 +1,6 @@
 package kshopov.web.eprescription.bootstrap;
 
-import kshopov.web.eprescription.model.Identifier;
+import kshopov.web.eprescription.model.IdentifierType;
 import kshopov.web.eprescription.services.IdentifierService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -20,13 +20,13 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if(identifierService.count() == 0) {
-            List<Identifier> identifiersList = Arrays.asList(
-                    new Identifier("ЕГН", "EGN", "v1.0.0", null),
-                    new Identifier("ЛНЧ", "LNZ", "v1.0.0", null),
-                    new Identifier("Социален номер - за чужди граждани", "SSN", "v1.0.0", null),
-                    new Identifier("Номер на паспорт", "PASS", "v1.0.0", null),
-                    new Identifier("Друг идентификатор", "Other", "v1.0.0", null),
-                    new Identifier("Новородено", "NBN", "v1.3.7", null)
+            List<IdentifierType> identifiersList = Arrays.asList(
+                    new IdentifierType("ЕГН", "EGN", "v1.0.0", null),
+                    new IdentifierType("ЛНЧ", "LNZ", "v1.0.0", null),
+                    new IdentifierType("Социален номер - за чужди граждани", "SSN", "v1.0.0", null),
+                    new IdentifierType("Номер на паспорт", "PASS", "v1.0.0", null),
+                    new IdentifierType("Друг идентификатор", "Other", "v1.0.0", null),
+                    new IdentifierType("Новородено", "NBN", "v1.3.7", null)
             );
             identifierService.saveAll(identifiersList);
         }
