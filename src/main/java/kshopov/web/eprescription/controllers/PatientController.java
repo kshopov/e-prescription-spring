219341eb0254c677ca3149580250db636a708dec
kshopov <krasimir.shopov@gmail.com>
2023-01-30 20:09:03 +0200

    identifier types loaded from database

--- src/main/java/kshopov/web/eprescription/controllers/PatientController.java
+++ src/main/java/kshopov/web/eprescription/controllers/PatientController.java
@@ -1,6 +1,8 @@
 package kshopov.web.eprescription.controllers;
 
+import kshopov.web.eprescription.model.IdentifierType;
 import kshopov.web.eprescription.model.Patient;
+import kshopov.web.eprescription.services.IdentifierService;
 import kshopov.web.eprescription.services.PatientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,19 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PatientController {
 
     private final PatientService patientService;
+    private final IdentifierService identifierService;
 
-    public PatientController(PatientService patientService) {
+    public PatientController(PatientService patientService, IdentifierService identifierService) {
         this.patientService = patientService;
+        this.identifierService = identifierService;
     }
 
     @GetMapping({  "addpatient"})
     public String addPatientForm(Model model) {
         System.out.println("Get add patient");
+
+        model.addAttribute("identifiers", getIdentifierTypes());
+
         return "patient/addpatient";
     }
 
@@ -34,6 +43,16 @@ public class PatientController {
         patientService.registerPatient(patient);
 
         return "redirect:/index";
+    }
+
+    private List<IdentifierType> getIdentifierTypes() {
+        List<IdentifierType> identifierTypes = new ArrayList<>();
+        Iterable<IdentifierType> identifiers = identifierService.getAll();
+        for (IdentifierType identifier : identifiers) {
+            identifierTypes.add(identifier);
+        }
+
+        return identifierTypes;
     }
 
 }
