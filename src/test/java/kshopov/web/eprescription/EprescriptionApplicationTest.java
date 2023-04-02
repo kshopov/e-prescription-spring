450b3761b80aae1fac5f95c602c9865881f2cb85
kshopov <krasimir.shopov@gmail.com>
2023-04-02 16:20:06 +0300

    added test for applcaiotn boot, generating unique email for doctor save test

---
+++ src/test/java/kshopov/web/eprescription/EprescriptionApplicationTest.java
@@ -0,0 +1,15 @@
+package kshopov.web.eprescription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EprescriptionApplicationTest {
+
+    @Test
+    void main() {
+        EprescriptionApplication.main(new String[] {});
+    }
+}
