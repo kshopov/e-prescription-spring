fd5adfc809f95f22458eb624edc7e3769f541727
kshopov <krasimir.shopov@gmail.com>
2023-07-13 08:43:49 +0300

    getAll method return List instead of Iterable

--- src/main/java/kshopov/web/eprescription/services/IdentifierService.java
+++ src/main/java/kshopov/web/eprescription/services/IdentifierService.java
@@ -2,13 +2,15 @@ package kshopov.web.eprescription.services;
 
 import kshopov.web.eprescription.model.IdentifierType;
 
+import java.util.List;
+
 public interface IdentifierService {
 
     public IdentifierType insertIdentifier(IdentifierType identifier);
 
     public Iterable<IdentifierType> saveAll(Iterable<IdentifierType> identifiers);
 
-    public Iterable<IdentifierType> getAll();
+    public List<IdentifierType> getAll();
 
     public long count();
 
