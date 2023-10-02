f578c391db04a1328ea94ff5c7c2c530281f7619
kshopov <krasimir.shopov@gmail.com>
2023-10-02 22:12:41 +0300

    remove import

--- src/test/java/kshopov/web/eprescription/EprescriptionApplicationTest.java
+++ src/test/java/kshopov/web/eprescription/EprescriptionApplicationTest.java
@@ -3,7 +3,6 @@ package kshopov.web.eprescription;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EprescriptionApplicationTest {
